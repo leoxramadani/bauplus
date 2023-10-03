752801ee65b6ac7f4c570646322d1eae205d363b
besirThor <besir.kurtishi@thorindustriesmk.com>
2023-10-03 16:10:46 +0200

    style: fixing drag n drop dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,7 +1,10 @@
+import GridLayout from '@/components/molecules/GridLayout/GridLayout';
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <GridLayout data={["1", "2", "3"]}   />
+  )
 };
 
 export default Dashboard;
