339e0e084bb2ca566d2003e9600fe754618b4753
besirThor <besir.kurtishi@thorindustriesmk.com>
2023-10-05 13:57:11 +0200

    fix: fix type finance errors

---
+++ pages/finance/invoice/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Invoice = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Invoice
--- pages/hr/index.tsx
+++ pages/hr/index.tsx
@@ -1,7 +1,12 @@
+import { Input } from '@/components/ui/input';
 import React from 'react';
 
 const Companies = () => {
-  return <div>Companies</div>;
+  return <div>
+    <Input value="besir" />
+     
+    
+  </div>;
 };
 
 export default Companies;
