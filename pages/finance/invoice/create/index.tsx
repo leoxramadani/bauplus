93131f3558fa7e50b026279552ccd99c36d0052e
ArdrinR <ardrin.rexhepi@thorindustriesmk.com>
2023-10-05 15:40:32 +0200

    fix: small bug fix

--- pages/finance/invoice/create/index.tsx
+++ pages/finance/invoice/create/index.tsx
@@ -1,38 +1,42 @@
-import { IInvoiceSchema, inflowInputColumns, invoiceSchema } from '@/lib/schema/finance';
-import React from 'react'
-import { Controller, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { IInvoiceSchema, invoiceSchema } from '@/lib/schema/finance';
+import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Input } from '@/components/ui/input';
 
 const Invoice = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<IInvoiceSchema>({
+    resolver: zodResolver(invoiceSchema),
+  });
 
-    const {
-        register,
-        handleSubmit,
-        control,
-        watch,
-        setValue,
-        formState: { errors },
-      } = useForm<IInvoiceSchema>({
-        resolver: zodResolver(invoiceSchema),
-      });
-
-
-      const handleInvoiceAdd = () => {
-        console.log("hello")
-      }
+  const handleInvoiceAdd = () => {
+    console.log('hello');
+  };
   return (
-    <div className='w-full'>
-        <form onSubmit={handleSubmit(handleInvoiceAdd)} className='grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 justify-center w-full h-full'>
-            <div className='flex flex-row justify-center items-center '>
-                <div className='bg-gray-100 h-full'>
-                    <span>INV#00</span>
-                </div>   
-            <Input className=' rounded-l-none outline-none border-0' {...register('invoiceNumber')}  />
-            </div>
-        </form>
+    <div className="w-full">
+      <form
+        onSubmit={handleSubmit(handleInvoiceAdd)}
+        className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 justify-center w-full h-full"
+      >
+        <div className="flex flex-row justify-center items-center ">
+          <div className="bg-gray-100 h-full">
+            <span>INV#00</span>
+          </div>
+          <Input
+            className=" rounded-l-none outline-none border-0"
+            {...register('invoiceNumber')}
+          />
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Invoice
+export default Invoice;
