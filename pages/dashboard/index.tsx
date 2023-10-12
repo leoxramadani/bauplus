2b822e1cc62be172ce037addfb3d1ef7ab92a5cb
Amar <amar.alimi@thorindustriesmk.com>
2023-10-12 16:59:22 +0200

    fix: small bug

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,10 +2,6 @@ import GridLayout from '@/components/molecules/GridLayout/GridLayout';
 import React from 'react';
 
 const Dashboard = () => {
-  if (typeof window !== 'undefined') {
-    // Code that uses localStorage
-    const someValue = localStorage.getItem('someKey');
-  }
   return <GridLayout data={['1', '2', '3']} />;
 };
 
