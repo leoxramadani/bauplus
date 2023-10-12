a24d764cf7ec4f50a48bc6b6bedd59099c8254c7
Amar <amar.alimi@thorindustriesmk.com>
2023-10-12 16:51:18 +0200

    feat: added static editable charts at dashboard

--- components/molecules/GridLayout/GridItem.tsx
+++ components/molecules/GridLayout/GridItem.tsx
@@ -1,39 +1,51 @@
 import { cn } from '@/lib/utils';
 import { Card } from '@tremor/react';
-import React, { forwardRef, CSSProperties, HTMLAttributes } from 'react';
-
+import React, {
+  forwardRef,
+  CSSProperties,
+  HTMLAttributes,
+} from 'react';
 
 interface CustomGridItemProps extends HTMLAttributes<HTMLDivElement> {
   style?: CSSProperties;
   children: React.ReactNode;
   className?: string;
-  
 }
 
-const GridItem = forwardRef<
-  HTMLDivElement,
-  CustomGridItemProps
->(
-  ({ style, className, onMouseDown, onMouseUp, onTouchEnd, children, ...props }, ref) => {
+const GridItem = forwardRef<HTMLDivElement, CustomGridItemProps>(
+  (
+    {
+      style,
+      className,
+      onMouseDown,
+      onMouseUp,
+      onTouchEnd,
+      children,
+      ...props
+    },
+    ref
+  ) => {
     return (
       <Card
         style={{ ...style }}
-        className={cn(className , 'flex flex-col justify-center items-center')}
+        className={cn(
+          className,
+          'flex flex-col justify-center items-center'
+        )}
         ref={ref}
         onMouseDown={onMouseDown}
         onMouseUp={onMouseUp}
         onTouchEnd={onTouchEnd}
         {...props}
       >
-         {/* <div className='grid-item__title bg-zinc-200 h-3 w-full  max-w-[80px] rounded-3xl absolute top-2 left-1/2 -translate-x-1/2 group-hover:opacity-100 cursor-move  opacity-0 transition-all'></div> */}
-      
-        {children} 
+        {/* <div className='grid-item__title bg-zinc-200 h-3 w-full  max-w-[80px] rounded-3xl absolute top-2 left-1/2 -translate-x-1/2 group-hover:opacity-100 cursor-move  opacity-0 transition-all'></div> */}
+
+        {children}
       </Card>
     );
   }
 );
 
 GridItem.displayName = 'GridItem';
-
 
 export default GridItem;
