6eadf711cb029f57fdc53cc7ed90c8a8950aa1f1
ArdrinR <ardrin.rexhepi@thorindustriesmk.com>
2023-10-16 15:28:31 +0200

    fix: small fix

--- lib/constants/endpoints/hr/departments.tsx
+++ lib/constants/endpoints/hr/departments.tsx
@@ -1,13 +1,13 @@
-import { BASE_URL } from "..";
+import { BASE_URL } from '..';
 
-export const GET_ALL_DEPARTMENTS = 
-    BASE_URL + `/api/DepartmentController/Department/GetAllDepartments`
+export const GET_ALL_DEPARTMENTS =
+  BASE_URL + `/api/Department/GetAllDepartments`;
 
-export const GET_SPECIFIC_DEPARTMENT = 
-BASE_URL + `/api/DepartmentController/Department/GetSpecificDepartment`
+export const GET_SPECIFIC_DEPARTMENT =
+  BASE_URL + `/api/Department/GetSpecificDepartment`;
 
-export const CREATE_DEPARTMENT = 
-    BASE_URL + `/api/DepartmentController/Department/CreateDepartment`
+export const CREATE_DEPARTMENT =
+  BASE_URL + `/api/Department/CreateDepartment`;
 
-export const UPDATE_DEPARTMENT = 
-    BASE_URL + `/api/DepartmentController/Department/UpdateDepartment`
+export const UPDATE_DEPARTMENT =
+  BASE_URL + `/api/Department/UpdateDepartment`;
