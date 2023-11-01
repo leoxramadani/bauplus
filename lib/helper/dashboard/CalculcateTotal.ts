a02cee4c7b332a3fe177fd87f34b32f674005411
Jasir Idriz <yasiridriz@gmail.com>
2023-11-01 15:30:07 +0000

    fix: merged from dev

--- lib/helper/dashboard/CalculcateTotal.ts
+++ lib/helper/dashboard/CalculcateTotal.ts
@@ -1,19 +1,19 @@
-import { useQuery } from '@apollo/client';
+// import { useQuery } from '@apollo/client';
 import { useEffect, useState } from 'react';
 import { TotalCalculated } from '../../queries/dashboard';
 
 export const CalculateTotal = () => {
-  const { loading, error, data } = useQuery(TotalCalculated);
+  // const { loading, error, data } = useQuery(TotalCalculated);
   const [myTotal, setTotal] = useState<number>();
-  useEffect(() => {
-    if (data) {
-      setTotal(data.allProfit.shumaTVSH);
-    }
-    if (error) {
-      console.log('error=', error);
-    }
-  }, [data, loading, error]);
-  // Access the decimal value from the data object
+  // useEffect(() => {
+  //   if (data) {
+  //     setTotal(data.allProfit.shumaTVSH);
+  //   }
+  //   if (error) {
+  //     console.log('error=', error);
+  //   }
+  // }, [data, loading, error]);
+  // // Access the decimal value from the data object
 
   return myTotal;
 };
