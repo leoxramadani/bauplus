56eb4c35cc1e4b2fded6244a8a18efcfb818bc82
Leotrim Ramadani <127308100+LeotrimThor@users.noreply.github.com>
2023-11-06 11:12:00 +0100

    fix: another bug fixed

--- pages/notices/index.tsx
+++ pages/notices/index.tsx
@@ -65,7 +65,11 @@ const Notices = () => {
         <div className="grid grid-cols-1 gap-4 gap-y-16 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
           {noticesData &&
             noticesData.map((item, index) => (
-              <NoticesCard cardData={item} key={index} />
+              <NoticesCard
+                cardData={item}
+                key={index}
+                noticesRefetch={noticesRefetch}
+              />
             ))}
         </div>
       </section>
