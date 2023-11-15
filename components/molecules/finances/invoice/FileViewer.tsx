0feb6018975b8dd701ee67397e188ffc2a8029a6
ArdrinR <ardrin.rexhepi@thorindustriesmk.com>
2023-11-15 16:53:34 +0100

    fix: small fix

--- components/molecules/finances/invoice/FileViewer.tsx
+++ components/molecules/finances/invoice/FileViewer.tsx
@@ -1,15 +1,27 @@
 'use client';
 
 interface FileViewerProps {
-  invoicePdf: string;
-  setInvoicePdf?: any;
+  previewFile: string;
+  setPreviewFile?: any;
 }
 const FileViewer = ({
-  invoicePdf,
-  setInvoicePdf,
+  previewFile,
+  setPreviewFile,
 }: FileViewerProps) => {
-  if (invoicePdf) {
-    return <iframe src={invoicePdf} width="100%" height="100%" />;
+  if (previewFile) {
+    console.log('there is a preview file');
+
+    if (typeof previewFile === 'string') {
+      return (
+        <img
+          src={previewFile}
+          className="h-auto w-auto"
+          alt="preview image"
+        />
+      );
+    }
+    
+    return <iframe src={previewFile} width="100%" height="100%" />;
   }
   return;
 };
