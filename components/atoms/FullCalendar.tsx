bf0a6487c48b50a4296f70449c7f38320dd7686f
besirThor <besir.kurtishi@thorindustriesmk.com>
2023-12-11 16:32:00 +0100

    feat: added delete holiday

--- components/atoms/FullCalendar.tsx
+++ components/atoms/FullCalendar.tsx
@@ -1,28 +1,95 @@
+import {
+  DELETE_HOLIDAY,
+  GET_ALL_HOLIDAYS,
+} from '@/lib/constants/endpoints/hr/holiday';
+import useData from '@/lib/hooks/useData';
+import useModal from '@/lib/hooks/useModal';
 import { IHoliday } from '@/lib/schema/hr/holiday/holiday';
 import dayGridMonth from '@fullcalendar/daygrid';
 import FC from '@fullcalendar/react';
+import axios from 'axios';
 import { addDays, format } from 'date-fns';
-import { ScrollArea } from '../ui/scroll-area';
+import { Trash } from 'lucide-react';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { Button } from '../ui/button';
+import Delete from './Delete';
+import Modal from './Modal';
 
 interface IFullCalendar {
   holidays: IHoliday[];
 }
 
 const FullCalendar = ({ holidays }: IFullCalendar) => {
-  console.log(holidays);
+  const { open, setOpen } = useModal();
+  const [deleting, setDeleting] = useState(false);
+
+  const { refetch: refetchHolidays } = useData<IHoliday[]>(
+    ['holidays'],
+    GET_ALL_HOLIDAYS
+  );
+
+  const handleDelete = async (id: string) => {
+    setDeleting(true);
+    try {
+      await axios.delete(DELETE_HOLIDAY, {
+        params: {
+          id: id,
+        },
+      });
+      toast.success('Holiday deleted successfully.');
+      refetchHolidays();
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong! Please try again.');
+    }
+    setDeleting(false);
+  };
+
+  function renderEventContent(eventInfo: any) {
+    return (
+      <div className=" group flex flex-row justify-between p-2">
+        <b>{eventInfo.event?.title}</b>
+        <Modal>
+          <Modal.Trigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="hidden h-max w-max p-1 group-hover:block"
+            >
+              <Trash size={10} className="text-gray-500" />
+            </Button>
+          </Modal.Trigger>
+          <Modal.Content
+            title="Delete a holiday"
+            description="This will delete the selected holiday! Are you sure you want to continue?"
+            className="max-w-xl"
+          >
+            <Delete
+              handleDelete={() => handleDelete(eventInfo.event.id)}
+              id={eventInfo.event.id}
+              deleting={deleting}
+            />
+          </Modal.Content>
+        </Modal>
+      </div>
+    );
+  }
   return (
-    holidays && (
-      <ScrollArea className="relative h-screen w-full">
+    <>
+      {holidays && (
         <FC
           headerToolbar={{
             left: 'prev,next',
             center: 'title',
-            right: 'dayGridMonth,dayGridWeek,dayGridDay', // user can switch between the two
+            // right: 'dayGridMonth,dayGridWeek,dayGridDay', // user can switch between the two
+            right: 'dayGridMonth', // user can switch between the two
           }}
           dayMaxEvents={3}
           plugins={[dayGridMonth]}
           initialView="dayGridMonth"
           weekends={true}
+          contentHeight={'100vh'}
           events={holidays?.map((holiday) => ({
             id: holiday.holidayId,
             title: holiday.name,
@@ -36,21 +103,19 @@ const FullCalendar = ({ holidays }: IFullCalendar) => {
 
               'yyyy-MM-dd HH:mm:ss'
             ),
+
+            // make the color a random color from the tailwind colors that end with 500
           }))}
           eventContent={renderEventContent}
-          height={'100vh'}
+          eventClick={(info) => {
+            // setOpen(true);
+            console.log(info);
+          }}
+          // height={'100vh'}
         />
-      </ScrollArea>
-    )
+      )}
+    </>
   );
 };
-
-function renderEventContent(eventInfo: any) {
-  return (
-    <div className=" flex flex-col p-0.5">
-      <b>{eventInfo.event?.title} </b>
-    </div>
-  );
-}
 
 export default FullCalendar;
--- lib/constants/endpoints/hr/holiday.ts
+++ lib/constants/endpoints/hr/holiday.ts
@@ -3,3 +3,4 @@ import { BASE_URL } from '..';
 export const GET_ALL_HOLIDAYS =
   BASE_URL + `/api/Holiday/GetAllHoliday`;
 export const CREATE_HOLIDAY = BASE_URL + `/api/Holiday/Add`;
+export const DELETE_HOLIDAY = BASE_URL + `/api/Holiday/Delete`;
