88ef3c900dbc028f29569fc546ae54ed621320fc
besirThor <besir.kurtishi@thorindustriesmk.com>
2023-12-21 14:13:23 +0100

    fix: bug fix

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -43,8 +43,8 @@ const DialogContent = React.forwardRef<
           {...props}
         >
           {children}
-          <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity data-[state=open]:bg-accent data-[state=open]:text-muted-foreground hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none">
-            <X className="h-4 w-4" />
+          <DialogPrimitive.Close className="absolute right-4 top-4 rounded-lg   opacity-70 transition-opacity data-[state=open]:bg-accent data-[state=open]:text-muted-foreground hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-red-100 focus:ring-offset-2 disabled:pointer-events-none">
+            <X strokeWidth={2} className="close" />
             <span className="sr-only">Close</span>
           </DialogPrimitive.Close>
         </DialogPrimitive.Content>
