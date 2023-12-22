cb7260195fbf0a74400d9c417c82dce3790418a2
Amar <amar.alimi@thorindustriesmk.com>
2023-12-22 16:57:42 +0100

    feat: attendance mapping works

--- lib/schema/hr/attendance/attendanceOptions.tsx
+++ lib/schema/hr/attendance/attendanceOptions.tsx
@@ -1,13 +1,15 @@
 import { z } from 'zod';
 
 export const databaseColumnsSchema = z.object({
-  employeeId: z.string(),
-  date: z.string(),
-  shiftId: z.string(),
-  timeIn: z.string(),
-  timeOut: z.string(),
-  status: z.string(),
-  note: z.string(),
+  columns: z
+    .object({
+      systemColumnName: z
+        .string()
+        .min(1, 'Please enter your employee ID column name'),
+      databaseColumnName: z.string(),
+    })
+    .array()
+    .optional(),
 });
 
 export type IDatabaseColumnsSchema = z.infer<
